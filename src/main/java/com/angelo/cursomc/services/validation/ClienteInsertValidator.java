dad4825e320e709725d4b32a3cb228e467563762
Angelo Carvalho <angelosuporteti@gmail.com>
2021-06-07 22:06:15 -0300

    Custom validation: wmail not repeated on customer entry

--- src/main/java/com/angelo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/angelo/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,18 +6,27 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.angelo.cursomc.domain.Cliente;
 import com.angelo.cursomc.domain.enums.TipoCliente;
 import com.angelo.cursomc.dto.ClienteNewDTO;
+import com.angelo.cursomc.repositories.ClienteRepository;
 import com.angelo.cursomc.resources.exception.FieldMessage;
 import com.angelo.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
@@ -27,6 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfouCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
