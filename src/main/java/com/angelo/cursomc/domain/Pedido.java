674377664862545ea122f8d22c62d44afc181e61
Angelo Carvalho <angelosuporteti@gmail.com>
2021-06-21 17:50:25 -0300

    Implementing order toString

--- src/main/java/com/angelo/cursomc/domain/Pedido.java
+++ src/main/java/com/angelo/cursomc/domain/Pedido.java
@@ -134,6 +134,24 @@ public class Pedido implements Serializable {
 		return true;
 	}
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(getInstante());
+		builder.append(", Cliente");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getStatus().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for (ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total: ");
+		builder.append(getValorTotal());
+		return builder.toString();
+	}
 
-	
 }
